66c2a23e2eb736c5baefd128bf9c5ad65c5cdd8f
RuiOliveira <RuiOliveira@DESKTOP-8RS6ENT>
2016-10-03 10:49:07 +0100

    add class and packages. Organização do projeto done

--- IR-maven/src/main/java/deti/ir/ranking/Ranking.java
+++ IR-maven/src/main/java/deti/ir/ranking/Ranking.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ir.maven;
+package deti.ir.ranking;
 
 /**
  *
  * @author RuiOliveira
  */
-public class Test {
-    public static void main(String[]args){
-        System.out.println("Test main"); 
-    }
+public class Ranking {
     
 }
