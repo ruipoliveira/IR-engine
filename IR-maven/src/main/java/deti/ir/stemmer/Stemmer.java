c6cbd0fd8892f078bcf73ed72f7280bde964d4cc
gabriel <gabriel.vieira@ua.pt>
2016-10-18 17:57:24 +0100

    Stemmer added to repository, now is just used them snowball jar added to pom.xml (dependencies)

--- IR-maven/src/main/java/deti/ir/stemmer/Stemmer.java
+++ IR-maven/src/main/java/deti/ir/stemmer/Stemmer.java
@@ -1,5 +1,7 @@
 package deti.ir.stemmer;
 
+import org.tartarus.snowball.*;
+
 /**
  * Universidade de Aveiro, DETI, Recuperação de Informação 
  * @author Gabriel Vieira, gabriel.vieira@ua.pt
@@ -7,11 +9,27 @@ package deti.ir.stemmer;
  */
 public class Stemmer {
     
+    /**
+     * Stemmer algorithm.
+     */
+    private final String algorithm;
+    
+    // podia ser passado por parametro mas apenas vai ser usado o english stemmer
     public Stemmer(){
-        
+        algorithm = "englishStemmer"; 
     }
     
     public String getStemmer(String token){
+        Class stemClass;
+        try {
+            stemClass = Class.forName("deti.ir.stemmer." + algorithm);
+            SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
+            stemmer.setCurrent(token);
+            if (stemmer.stem()){
+                return stemmer.getCurrent();
+            }
+                
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {}
         return token;
     }
 }
