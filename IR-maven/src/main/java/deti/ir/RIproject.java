4b1cc9c2129dba73290b55b326cd841296732a4d
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-12-21 00:24:19 +0000

    menu search or indexer

--- IR-maven/src/main/java/deti/ir/RIproject.java
+++ IR-maven/src/main/java/deti/ir/RIproject.java
@@ -1,8 +1,10 @@
 package deti.ir;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Universidade de Aveiro, DETI, Recuperação de Informação 
@@ -22,27 +24,41 @@ public class RIproject {
      */
     public static void main(String[]args) throws FileNotFoundException, IOException, Exception{
        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         
         File fdir = new File(args[0]);
         File fstp = new File(args[1]);
-        int memory = Integer.parseInt(args[2]); 
-        /*
-        if (fdir.exists() && fdir.isDirectory() && fstp.exists() && (memory >= 1 && memory <= 4096)) {
-            DocumentProcessor dp = new DocumentProcessor(args[0], args[1], memory); 
-            dp.start();
-        }else{
-            System.out.println("Parametros incorretos."); 
-        }
-        */
+        int memory = Integer.parseInt(args[2]);
         
-            
-        if (fdir.exists() && fdir.isDirectory() && fstp.exists() && (memory >= 1 && memory <= 4096)) {
-            SearchProcessor search = new SearchProcessor(args[0], args[1]); 
-            search.start();
-            
-        }else{
-            System.out.println("Parametros incorretos."); 
+        System.out.println("1-indexer files\n2-search query\nInsira a sua opção: ");
+        
+        String op = br.readLine();
+        
+        
+        if (Integer.parseInt(op) ==1){
+            if (fdir.exists() && fdir.isDirectory() && fstp.exists() && (memory >= 1 && memory <= 4096)) {
+                DocumentProcessor dp = new DocumentProcessor(args[0], args[1], memory); 
+                dp.start();
+            }else{
+                System.out.println("Parametros incorretos."); 
+            }
         }
+        
+       
+        else if (Integer.parseInt(op) ==2){
+            if (fdir.exists() && fdir.isDirectory() && fstp.exists() && (memory >= 1 && memory <= 4096)) {
+                SearchProcessor search = new SearchProcessor(args[0], args[1]); 
+                search.start();
+
+            }else{
+                System.out.println("Parametros incorretos."); 
+            }
+        }
+        else{
+            System.out.println("Opção inválida!"); 
+        }
+        
         
         
    
